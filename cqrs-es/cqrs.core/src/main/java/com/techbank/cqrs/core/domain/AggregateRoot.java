3b3c1bf0aa4ecce4beba9e6d73eb4cd2bd6b5e4a
david <davidtrajkovski11@gmail.com>
2024-06-10 23:44:39 +0200

    creating AggregateRoot

---
+++ cqrs-es/cqrs.core/src/main/java/com/techbank/cqrs/core/domain/AggregateRoot.java
@@ -0,0 +1,63 @@
+package com.techbank.cqrs.core.domain;
+
+import com.techbank.cqrs.core.events.BaseEvent;
+
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public abstract class AggregateRoot {
+    protected String id;
+    private int version = -1;
+    private final List<BaseEvent> changes = new ArrayList<>();
+    private final Logger logger = Logger.getLogger(AggregateRoot.class.getName());
+
+    public String getId() {
+        return this.id;
+    }
+
+    public int getVersion() {
+        return this.version;
+    }
+
+    public void setVersion(int version) {
+        this.version = version;
+    }
+
+    public List<BaseEvent> getUncomittedChanges() {
+        return this.changes;
+    }
+
+    public void markChangesAsCommitted() {
+        this.changes.clear();
+    }
+
+    protected void applyChange(BaseEvent event, Boolean isNewEvent) {
+        try {
+            var method = getClass().getDeclaredMethod("apply", event.getClass()); // reflexion
+            method.setAccessible(true);
+            method.invoke(this, event);
+        } catch (NoSuchMethodException e) {
+            logger.log(Level.WARNING, MessageFormat.format("The apply method was not found in the aggregate for {0}",
+                    event.getClass().getName()));
+        } catch (Exception e) {
+            logger.log(Level.SEVERE, MessageFormat.format("Error applying event to aggregate ", e));
+        } finally {
+            if (isNewEvent) {
+                changes.add(event);
+            }
+        }
+
+    }
+
+    public void raiseEvent(BaseEvent event) {
+        applyChange(event, true);
+    }
+
+    public void replayEvents(Iterable<BaseEvent> events) {
+        events.forEach(event -> applyChange(event, false));
+    }
+
+}
