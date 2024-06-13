2eb8244122cb2430f2b7f863959adb903a0bb766
david <davidtrajkovski11@gmail.com>
2024-06-13 23:24:56 +0200

    adding domain bankAccount class and repository

---
+++ bank-account/account.query/src/main/java/com/techbank/account/query/domain/AccountRepository.java
@@ -0,0 +1,15 @@
+package com.techbank.account.query.domain;
+
+import com.techbank.cqrs.core.domain.BaseEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface AccountRepository extends JpaRepository<BankAccount, String> {
+    Optional<BankAccount> findByAccountHolder(String accountHolder);
+    List<BaseEntity> findByBalanceGreaterThan(double balance);
+    List<BaseEntity> findByBalanceLessThan(double balance);
+}
---
+++ cqrs-es/cqrs.core/src/main/java/com/techbank/cqrs/core/domain/BaseEntity.java
@@ -0,0 +1,4 @@
+package com.techbank.cqrs.core.domain;
+
+public abstract class BaseEntity {
+}
