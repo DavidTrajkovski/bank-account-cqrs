b9de875184a06b374cbb436c8aa6cd2c3d4c3519
david <davidtrajkovski11@gmail.com>
2024-06-14 00:09:15 +0200

    AccountEventHandler

---
+++ bank-account/account.query/src/main/java/com/techbank/account/query/infrastructure/handlers/EventHandler.java
@@ -0,0 +1,13 @@
+package com.techbank.account.query.infrastructure.handlers;
+
+import com.techbank.account.common.events.AccountClosedEvent;
+import com.techbank.account.common.events.AccountOpenedEvent;
+import com.techbank.account.common.events.FundsDepositedEvent;
+import com.techbank.account.common.events.FundsWithdrawnEvent;
+
+public interface EventHandler {             // can this be done with 1 func BaseEvent instead of 4 separate
+    void on(AccountOpenedEvent event);
+    void on(FundsDepositedEvent event);
+    void on(FundsWithdrawnEvent event);
+    void on(AccountClosedEvent event);
+}
