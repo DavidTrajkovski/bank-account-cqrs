847da0dd487a9aac837435ef6285d27c9a3ffc32
david <davidtrajkovski11@gmail.com>
2024-06-13 22:14:00 +0200

    defining and registering handles

---
+++ bank-account/account.cmd/src/main/java/com/techbank/account/cmd/api/commands/AccountCommandHandler.java
@@ -0,0 +1,46 @@
+package com.techbank.account.cmd.api.commands;
+
+import com.techbank.account.cmd.domain.AccountAggregate;
+import com.techbank.cqrs.core.handlers.EventSourcingHandler;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AccountCommandHandler implements CommandHandler {
+
+    private final EventSourcingHandler<AccountAggregate> eventSourcingHandler;
+
+    public AccountCommandHandler(EventSourcingHandler<AccountAggregate> eventSourcingHandler) {
+        this.eventSourcingHandler = eventSourcingHandler;
+    }
+
+    @Override
+    public void handle(OpenAccountCommand command) {
+        var aggregate = new AccountAggregate(command);
+        eventSourcingHandler.save(aggregate);
+    }
+
+    @Override
+    public void handle(DepositFundsCommand command) {
+        var aggregate = eventSourcingHandler.getById(command.getId());
+        aggregate.depositFunds(command.getAmount());
+        eventSourcingHandler.save(aggregate);
+    }
+
+    @Override
+    public void handle(WithdrawFundsCommand command) {
+        var aggregate = eventSourcingHandler.getById(command.getId());
+        if (command.getAmount() > aggregate.getBalance()) {
+            throw new IllegalStateException("Withdrawal declined, insufficient funds!");
+        }
+        aggregate.withdrawFunds(command.getAmount());
+        eventSourcingHandler.save(aggregate);
+    }
+
+    @Override
+    public void handle(CloseAccountCommand command) {
+        var aggregate = eventSourcingHandler.getById(command.getId());
+        aggregate.closeAccount();
+        eventSourcingHandler.save(aggregate);
+    }
+
+}
---
+++ bank-account/account.cmd/src/main/java/com/techbank/account/cmd/api/commands/CommandHandler.java
@@ -0,0 +1,8 @@
+package com.techbank.account.cmd.api.commands;
+
+public interface CommandHandler {                   // zasto ova ne se napravi genericko posto site ovie nasleduvaat od BaseCommand
+    void handle(OpenAccountCommand command);
+    void handle(DepositFundsCommand command);
+    void handle(WithdrawFundsCommand command);
+    void handle(CloseAccountCommand command);
+}
